133d85123c0d956b1c3db343c1ec35ccff7243ed
David Cingolani <cingolanidavid@gmail.com>
2020-12-25 15:16:17 -0300

    add feature of swith between dialogs

--- src/containers/Payroll.jsx
+++ src/containers/Payroll.jsx
@@ -1,35 +1,26 @@
-import React, { Fragment, lazy, Suspense } from "react";
-import {payrolls,incomesDeductions } from "./utils/data"
-// Material UI
-import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableRow from "@material-ui/core/TableRow";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import {
-  KeyboardDatePicker,
-  MuiPickersUtilsProvider,
-} from "@material-ui/pickers";
-
-// Date-FNS
-// import { format as formatDate } from "date-fns";
-import DateFnsUtils from "@date-io/date-fns";
+import React, { lazy, Suspense } from "react";
+import { incomesDeductions } from "../utils/data";
+import { TablePayroll } from "../components";
 
 // Lazy Load
-const IncomeDeduction = lazy(() => import("./IncomeDeduction"));
+const IncomeDeduction = lazy(() =>
+  import("../components/IncomeDeductionDialog/IncomeDeductionDialog")
+);
+
+const IncomeDialog = lazy(() =>
+  import("../components/IncomeDialog/IncomeDialog")
+);
+
+const DeductionDialog = lazy(() =>
+  import("../components/DeductionDialog/DeductionDialog")
+);
 
 const Payroll = () => {
-
-
   const [openIncomeDeductionDialog, setIncomeDeductionDialog] = React.useState(
     false
   );
+  const [openIncomeDialog, setOpenIncomeDialog] = React.useState(false);
+  const [openDeductionDialog, setOpenDeductionDialog] = React.useState(false);
   const [selectedDate, setSelectedDate] = React.useState(null);
   const [selectedDetails, setSelectedDetails] = React.useState(null);
 
@@ -43,117 +34,33 @@ const Payroll = () => {
       <Suspense fallback={<h3>Loading...</h3>}>
         <IncomeDeduction
           open={openIncomeDeductionDialog}
-          details={selectedDetails}
-          onAgree={() => setIncomeDeductionDialog(false)}
-          onDisagree={() => setIncomeDeductionDialog(false)}
+          completeName={selectedDetails?.name}
+          openDialog={() => setIncomeDeductionDialog(false)}
+          onClose={() => setIncomeDeductionDialog(false)}
+          openIncomeDialog={() => setOpenIncomeDialog(true)}
+          openDeductionDialog={() => setOpenDeductionDialog(true)}
+        />
+      </Suspense>
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <IncomeDialog
+          open={openIncomeDialog}
+          onClose={() => setOpenIncomeDialog(false)}
+          openIncomeDeductionDialog={() => setIncomeDeductionDialog(true)}
+        />
+      </Suspense>
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <DeductionDialog
+          open={openDeductionDialog}
+          onClose={() => setOpenDeductionDialog(false)}
+          openIncomeDeductionDialog={() => setIncomeDeductionDialog(true)}
         />
       </Suspense>
 
-      <Fragment>
-        <Grid container spacing={2}>
-          <Grid item lg={12} md={12} sm={12} xs={12}>
-            <Typography variant="body1">Date</Typography>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-              <KeyboardDatePicker
-                autoOk
-                fullWidth
-                format="MM/dd/yyyy"
-                inputVariant="outlined"
-                variant="outlined"
-                size="small"
-                style={{
-                  background: "#fff",
-                }}
-                values={selectedDate}
-                onChange={(val) => {
-                  setSelectedDate(val);
-                }}
-              />
-            </MuiPickersUtilsProvider>
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={2}>
-          <Grid item lg={12} md={12} sm={12} xs={12}>
-            {payrolls && payrolls.length > 0 ? (
-              <Fragment>
-                <TableContainer component={Paper}>
-                  <Table stickyHeader>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>Subdepartment</TableCell>
-                        <TableCell>Employee Code</TableCell>
-                        <TableCell>Employee Name</TableCell>
-                        <TableCell align="right">Days/Hours</TableCell>
-                        <TableCell align="right">Gross</TableCell>
-                        <TableCell align="right">Net</TableCell>
-                        <TableCell>Action</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {payrolls.map((payroll, index) => (
-                        <TableRow key={index}>
-                          <TableCell>{payroll.SubDept}</TableCell>
-                          <TableCell>{payroll.DatacenterCode}</TableCell>
-                          <TableCell>{payroll.CompleteName}</TableCell>
-                          <TableCell align="right">{payroll.Days}</TableCell>
-                          <TableCell align="right">{payroll.Gross}</TableCell>
-                          <TableCell align="right">{payroll.Net}</TableCell>
-                          <TableCell>
-                            <Button
-                              color="primary"
-                              variant="contained"
-                              onClick={() => IncomeDeductionDialog(payroll)}
-                            >
-                              View
-                            </Button>
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                    <TableBody>
-                      <TableRow>
-                        <TableCell colSpan={3}></TableCell>
-                        <TableCell variant="head" align="right">
-                          Total
-                        </TableCell>
-                        <TableCell align="right">
-                          {payrolls.reduce(
-                            (totalGross, gross) =>
-                              (totalGross += parseInt(gross.Gross)),
-                            0
-                          )}
-                        </TableCell>
-                        <TableCell align="right">
-                          {payrolls.reduce(
-                            (totalNet, net) => (totalNet += parseInt(net.Net)),
-                            0
-                          )}
-                        </TableCell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-
-                <Grid container>
-                  <Grid item lg={12} md={12} sm={12} xs={12}>
-                    <Typography variant="body1">Remarks</Typography>
-                    <TextField />
-                  </Grid>
-                </Grid>
-
-                <Grid container justify="flex-end">
-                  <Button color="primary" variant="contained">
-                    FINAL SAVE
-                  </Button>
-                </Grid>
-              </Fragment>
-            ) : (
-              ""
-            )}
-          </Grid>
-        </Grid>
-      </Fragment>
+      <TablePayroll
+        selectedDate={selectedDate}
+        setSelectedDate={setSelectedDate}
+        IncomeDeductionDialog={IncomeDeductionDialog}
+      />
     </div>
   );
 };
